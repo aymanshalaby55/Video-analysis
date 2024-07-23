815d781eb090aa01a345d7b728cb093a6f323817
he4a00 <https://github.com/he4a00>
2024-07-23 16:45:15 +0300

    fixed get user route

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const { verifyTokenAndAdmin, protect } = require('../middleware/verifyToken');
+const { verifyTokenAndAdmin, protect } = require("../middleware/verifyToken");
 
-const userConrtollers = require('../controllers/userControllers');
+const userConrtollers = require("../controllers/userControllers");
 
-router.post('/register', userConrtollers.register);
-router.post('/login', userConrtollers.login);
+router.post("/register", userConrtollers.register);
+router.post("/login", userConrtollers.login);
 
 router.use(protect);
-router.get('/', verifyTokenAndAdmin, userConrtollers.getAllUsers);
-router.post('/logout', userConrtollers.logout);
-router.patch('/edit/:userId', userConrtollers.updatedUser);
-router.get('getUser/:id', userConrtollers.getUser);
+router.get("/", verifyTokenAndAdmin, userConrtollers.getAllUsers);
+router.post("/logout", userConrtollers.logout);
+router.patch("/edit/:userId", userConrtollers.updatedUser);
+router.get("/getUser/:id", userConrtollers.getUser);
 
 module.exports = router;
